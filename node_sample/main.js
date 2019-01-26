100245fc4c107ac94961050255a4ac817c181ca0
Norio <umanari145@gmail.com>
2019-01-27 01:12:56 +0900

    コミットもれ

--- node_sample/main.js
+++ node_sample/main.js
@@ -1,6 +1,16 @@
 
 const url = "https://paiza.jp/career/job_offers"
 const GetFile = require('./scraping/getfile.js');
+const Recruit = require('./scraping/registRecruit.js');
+const loadenv = require('node-env-file');
+const DButil =  require('./util/dbUtil.js');
+loadenv('.env')
+
+const dbUtil = new DButil(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS);
+
+
 const getfile = new GetFile(url);
+const recruit = new Recruit(dbUtil);
+
 let total_recuit_info_arr= getfile.get_recruits()
-console.log(total_recuit_info_arr)
+recruit.registRecruit(total_recuit_info_arr)
