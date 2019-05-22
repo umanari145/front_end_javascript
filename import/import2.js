42ff4680ca20b6f06a0226fb2fbd0831ef4833d1
Norio <umanari145@gmail.com>
2019-05-23 00:36:59 +0900

    一括でクラスを呼ぶ呼び出し方

---
+++ import/import2.js
@@ -0,0 +1,7 @@
+import * as Lib2 from './lib2/index.js'
+
+const member = new Lib2.MemberClass(99,'default');
+member.printName()
+
+const person = new Lib2.PersonClass(98,'default2');
+person.printName()
---
+++ import/lib2/index.js
@@ -0,0 +1,8 @@
+import {MemberClass} from './memberClass.js'
+import {PersonClass} from './personClass.js'
+
+
+export{
+    MemberClass,
+    PersonClass
+}
