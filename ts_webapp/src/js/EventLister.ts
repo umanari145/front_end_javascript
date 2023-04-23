9dec0587d687240b2a1ff3c18097c4955fddc3e7
umanari145 <umanari145@gmail.com>
2023-04-23 12:24:33 +0900

    linterとEventLinsterクラスを追加

---
+++ ts_webapp/src/js/EventLister.ts
@@ -0,0 +1,40 @@
+// typeはinterface 
+// https://typescriptbook.jp/reference/object-oriented/interface/interface-vs-type-alias
+type Listeners = {
+    // 動的プロパティ
+    [id: string]: {
+        event: string
+        element: HTMLElement
+        handler: (e: Event) => void
+    }
+}  
+
+export class EventLister {
+    private readonly linsteners: Listeners = {}
+    
+    add(listenerId: string, event: string, element: HTMLElement, handler: (e :Event) => void) {
+        
+        this.linsteners[listenerId] = {
+            event,
+            element,
+            handler
+        }
+
+        element.addEventListener(event, handler)
+    }
+    
+    remove(listenerId: string):void {
+        const listener = this.linsteners[listenerId];
+
+        if (!listener) {
+            return
+        }
+
+        listener.element.removeEventListener(
+            listener.event, 
+            listener.handler
+        )
+
+        delete this.linsteners[listenerId]
+    }
+}
